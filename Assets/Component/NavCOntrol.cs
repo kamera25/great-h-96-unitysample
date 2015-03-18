dc38cb236c6ac9a453c044b1d13c3accc1f9ca16
Takahiro Nakaoku <giraffe_rakugaki@yahoo.co.jp>
2015-03-18 23:16:10 +0900

    最初のコミット

---
+++ Assets/Component/NavCOntrol.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavCOntrol : MonoBehaviour {
+
+	public NavMeshAgent navMesh;
+	public Transform AimObj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		navMesh.SetDestination (AimObj.position);
+	}
+}
